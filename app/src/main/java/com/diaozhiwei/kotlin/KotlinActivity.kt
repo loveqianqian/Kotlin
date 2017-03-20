8102748974027a36e5c14cb6d37e235514f78547
loveqianqian <diaozhiwei2k@163.com>
2017-03-20 23:09:02 +0800

    Initial commit

---
+++ app/src/main/java/com/diaozhiwei/kotlin/KotlinActivity.kt
@@ -0,0 +1,14 @@
+package com.diaozhiwei.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_kotlin.*
+
+class KotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+        text_hello.text=getString(R.string.self_name)
+    }
+}
